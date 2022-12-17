b0eea32737341e25ca528af5bed2b2237535fd51
pgusew <pgusew@team.amocrm.com>
2022-12-17 11:05:56 +0300

    fixed status name and tag name

--- hf_integration_sample/api/controllers/applicant_hook.py
+++ hf_integration_sample/api/controllers/applicant_hook.py
@@ -16,12 +16,12 @@ async def process_applicant_hook(hook_data: ApplicantHookRequest):
     applicant_id = hook_data.event.applicant.id
     try:
         if applicant_log.type == "STATUS":
-            if applicant_log.status.name == "Нанят":
+            if applicant_log.status.name == "hired":
                 apiclient = get_client()
                 all_organization_tags = await apiclient.get_all_tags()
                 all_applicant_tags_id = await apiclient.get_all_applicant_tags(applicant_id)
                 for tag in all_organization_tags:
-                    if tag.name == "hired":
+                    if tag.name == "нанят":
                         hired_tag_id = tag.id
                         if hired_tag_id not in all_applicant_tags_id.tags:
                             all_applicant_tags_id.tags.append(hired_tag_id)
@@ -32,7 +32,7 @@ async def process_applicant_hook(hook_data: ApplicantHookRequest):
                             logger.info("Skip webhook, the applicant already has the 'Hired' tag")
                             return all_applicant_tags_id
                 logger.info("'Hired' tag doesn't exist, create and add it to applicant")
-                hired_tag = await apiclient.create_tag(dto.Tag(name="hired", color="00ad3b"))
+                hired_tag = await apiclient.create_tag(dto.Tag(name="нанят", color="00ad3b"))
                 all_applicant_tags_id.tags.append(hired_tag.id)
                 return await apiclient.update_applicant_tags(applicant_id, all_applicant_tags_id)
             else:
